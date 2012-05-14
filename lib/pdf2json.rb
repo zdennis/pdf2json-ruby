e8a7d64b8a4de839d612cbbb1e9344ddeb5006df
Zach Dennis <zach.dennis@gmail.com>
2012-05-14 10:14:12 -0400

    Adding #pdf2json_binary_path to Pdf2Json::Helpers module

--- lib/pdf2json.rb
+++ lib/pdf2json.rb
@@ -1,9 +1,13 @@
 module Pdf2Json
   BIN_DIR = File.dirname(__FILE__) + '/../bin/'
   
-  module BinaryHelpers
+  module Helpers
     def pdf2json(*args)
       `#{Pdf2Json::BIN_DIR} #{args.join(' ')}`
+    end
+
+    def pdf2json_binary_path(*args)
+      File.join Pdf2Json::BIN_DIR, "pdf2json"
     end
   end
 end
